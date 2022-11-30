9c818730035818a09044e1c4012c18591403f8b4
Isabella Costabile <costabileisa@gmail.com>
2022-11-30 12:04:18 -0500

    finished all code in one sitting and forgot to commit along the way

--- src/index.js
+++ src/index.js
@@ -2,18 +2,31 @@ window.addEventListener('DOMContentLoaded', () => {
     testingapi();
 })
 
-function testingapi() {
+window.addEventListener('click', () => {
+    document.body.style.backgroundColor = "#ffd1dc"
+})
+
+window.addEventListener('keydown', (event) => {
+    document.getElementById("api-list").innerHTML = "";
+    testingapi(event.key);
+})
+
+function testingapi(input) {
     const ul = document.getElementById('api-list');
 
     fetch('https://api.publicapis.org/entries')
     .then(res => res.json())
     .then(data => {
         data.entries.forEach(element => {
-            console.log(element);
-            let li = document.createElement("li");
-            li.innerText = element.API;
-            li.id = element.API
-            ul.append(li);
+            if (!element.Auth) {
+                if (input == null || element.API.charAt(0).toUpperCase() == input.toUpperCase()) {
+                    console.log(element)
+                    let li = document.createElement("li");
+                    li.innerText = element.API;
+                    li.id = element.API;
+                    ul.append(li);
+                }
+            }
         })
     });
 };
