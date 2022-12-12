0b70e253c988f17649d47432a011a7abe3176da5
Isabella Costabile <costabileisa@gmail.com>
2022-12-12 00:48:49 -0500

    made it so links can be clicked

--- src/index.js
+++ src/index.js
@@ -1,40 +1,46 @@
-const style = { "textColor": "magenta", "fontSize": "125%", "backgroundColor": "#ffd1dc" };
-
 document.addEventListener('DOMContentLoaded', () => {
     listAPI();
 })
 
 // on click, background of page will turn pink
 document.addEventListener('click', () => {
-    document.body.style.backgroundColor = style.backgroundColor;
+    document.body.classList.add("changeColor");
 })
 
 // on keypress, will display only APIs that match that letter
 document.addEventListener('keydown', (event) => {
-    // if key pressed is escape, return to default list
-    if (event.key === "Escape") listAPI(null);
+    // reset the list
     document.getElementById("api-list").innerHTML = "";
+    // if key pressed is escape, return to default list
+    if (event.key === "Escape") return listAPI(null);
+    
     listAPI(event.key);
 })
 
-document.addEventListener('mouseover', (e) => {
-    changeStyle(e.target.id, style.fontSize, style.textColor)
-})
-document.addEventListener("mouseout", (e) => {
-    changeStyle(e.target.id, "", "")
-})
+document.addEventListener('mouseover', (e) => style(e, "add"))
+document.addEventListener("mouseout", (e) => style(e, "remove"))
 
-// change styling (font size & color)
-function changeStyle(element, fontSize, color) {
-    const api = document.getElementById(element);
-    if (!api || api.id === "api-list") return;
-    api.style.fontSize = fontSize;
-    api.style.color = color;
+function style(event, style) {
+    const className = event.target.className.split(" ")[0]
+    if (!className) return;
+
+    const collection = document.getElementsByClassName(className)
+
+    for (let i = 0; i < collection.length; i++) {
+        const element = collection.item(i)
+        if (style === "remove") {
+            element.classList.remove("styled")
+        }
+        if (style === "add") {
+            element.classList.add("styled")
+        }
+    }
+
 }
 
 function listAPI(input) {
     // grabs the ul from index.html that the lis will be added to
-    const ul = document.getElementById('api-list');
+    const apiList = document.getElementById('api-list');
 
     // fetches all entries from the api
     fetch('https://api.publicapis.org/entries')
@@ -45,26 +51,32 @@ function listAPI(input) {
             if (!element.Auth) {
                 // outputs both lowercase and uppercase for the keydown event
                 if (input == null || element.API.charAt(0).toUpperCase() == input.toUpperCase()) {
-                    const li = document.createElement("li");
-                    // filters the original api object to just the name of the api, the description, and a link
-                    const filtered = ["API", "Description", "Link"].reduce((result, key) => { result[key] = element[key]; return result; }, {});
-                    // loops to add info so html can display
-                    let info = "";
-                    for (let x in filtered) {
-                        info += filtered[x] + "<br>";
-                    };
-                    li.innerHTML = info;
+                    const className = element.Description.split(" ").join("-")
+                    let div = document.createElement("div");
+                    let ul = document.createElement("ul")
 
-                    // adds the name of the API as the id to each element
-                    // if the id already exists, adds 1 to the end of it
-                    if (document.getElementById(element.API)) {
-                        li.id = element.API + 1;
-                    } else {
-                        li.id = element.API;
-                    };
+                    let apiName = document.createElement("li")
+                    apiName.innerHTML = element.API
+                    apiName.className = className
 
-                    // appends the filtered object to the ul
-                    ul.append(li);
+                    let apiDescription = document.createElement("li")
+                    apiDescription.innerHTML = element.Description
+                    apiDescription.className = className
+
+                    let apiLink = document.createElement("li")
+                    let link = document.createElement("a")
+                    link.href = element.Link
+                    link.innerHTML = element.Link
+                    link.className = className
+                    apiLink.className = className
+
+                    apiLink.append(link)
+
+                    ul.append(apiName, apiDescription, apiLink)
+
+                    // appends the ul to the div
+                    div.append(ul);
+                    apiList.append(div)
                 }
             }
         })
