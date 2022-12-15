e523d1699c632fd9389826840de3d4abf756e61d
Isabella Costabile <costabileisa@gmail.com>
2022-12-15 13:22:31 -0500

    added a search bar

--- src/index.js
+++ src/index.js
@@ -1,43 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
     listAPI();
-
-    // on click, background of page will turn pink
-    document.addEventListener('click', () => {
-        document.body.classList.add("changeColor");
-    })
-
-    // on keypress, will display only APIs that match that letter
-    document.addEventListener('keydown', (event) => {
-        // reset the list
-        document.getElementById("api-list").innerHTML = "";
-        // if key pressed is escape, return to default list
-        if (event.key === "Escape") return listAPI(null);
-    
-        listAPI(event.key);
-    })
-
-    document.addEventListener('mouseover', (e) => style(e, "add"))
-    document.addEventListener("mouseout", (e) => style(e, "remove"))
+    search();
+    changeColor();
+    hoverStyle();
+    // addComment()
 })
 
-function style(event, style) {
-    const className = event.target.className.split(" ")[0]
-    if (!className) return;
-    if (event.target.nodeName !== "LI" && event.target.nodeName !== "A") return;
+// function addComment() {
+//     const form = document.getElementById("comment-form");
 
-    const collection = document.getElementsByClassName(className)
+//     // creates a div for the comments
+//     let commentsDiv = document.createElement("div");
+//     commentsDiv.id = "comments-div"
 
-    for (let i = 0; i < collection.length; i++) {
-        const element = collection.item(i)
-        if (style === "remove") {
-            element.classList.remove("styled")
-        }
-        if (style === "add") {
-            element.classList.add("styled")
-        }
-    }
+//     form.append(commentsDiv)
 
-}
+//     // creates a new h2 header for comments
+//     let commentsHeader = document.createElement("h2")
+//     commentsHeader.id = "comments";
+//     commentsHeader.innerHTML = "Comments"
+    
+//     commentsDiv.append(commentsHeader)
+
+//     // creates a new ul for the comments to be added to
+//     let ul = document.createElement("ul")
+//     ul.id = "comments-list"
+//     ul.style.listStyleType = "none";
+
+//     commentsDiv.append(ul)
+
+//     // listen for form submit
+//     form.addEventListener("submit", event => {
+//         event.preventDefault()
+//         // get the comment value
+//         let comment = document.getElementById("comment")
+        
+//         // create new p and add the comment
+//         let li = document.createElement("li");
+//         li.innerHTML = (comment.value)        
+//         // append the comment to the comments section
+//         ul.append(li)
+
+//         comment.value = "";
+//         comment.innerHTML = comment.value;
+//     })
+// }
 
 async function listAPI(input) {
     // grabs the ul from index.html that the lis will be added to
@@ -51,7 +58,7 @@ async function listAPI(input) {
             // filters only for elements with no auth key
             if (!element.Auth) {
                 // outputs both lowercase and uppercase for the keydown event
-                if (input == null || element.API.charAt(0).toUpperCase() == input.toUpperCase()) {
+                if (input == null || input === "" || element.API.toLowerCase().startsWith(input.toLowerCase()) === true) {
                     const className = element.Description.split(" ").join("-")
                     let ul = document.createElement("ul")
 
@@ -79,5 +86,49 @@ async function listAPI(input) {
                 }
             }
         })
-    });
-};
+    })
+}
+
+function search() {
+    const form = document.getElementById("search-box")
+    form.addEventListener("keyup", event => {
+        const searchContent = document.getElementById("search").value
+
+        document.getElementById("api-list").innerHTML = "";
+
+        if (event.key === "Escape") return listAPI(null);
+
+        listAPI(searchContent)
+    })
+}
+
+function changeColor() {
+    // on click, background of page will turn pink
+    document.addEventListener('click', () => {
+        document.body.classList.add("changeColor");
+    })
+}
+
+function style(event, style) {
+    const className = event.target.className.split(" ")[0]
+    if (!className) return;
+    if (event.target.nodeName !== "LI" && event.target.nodeName !== "A") return;
+
+    const collection = document.getElementsByClassName(className)
+
+    for (let i = 0; i < collection.length; i++) {
+        const element = collection.item(i)
+        if (style === "remove") {
+            element.classList.remove("styled")
+        }
+        if (style === "add") {
+            element.classList.add("styled")
+        }
+    }
+
+}
+
+function hoverStyle() {
+    document.addEventListener('mouseover', (e) => style(e, "add"))
+    document.addEventListener("mouseout", (e) => style(e, "remove"))
+}
